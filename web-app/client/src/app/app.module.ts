52dc7997025005442b726ebf0c34f43f5c0e7b06
Rieha <rieha.burrell@ibm.com>
2019-04-03 14:18:42 +0100

    removed unnecessary queryCar service
    Signed-off-by: Rieha <rieha.burrell@ibm.com>

--- web-app/client/src/app/app.module.ts
+++ web-app/client/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { QueryAllCarsComponent } from './query-all-cars/query-all-cars.component';
 import { ApiService } from './api.service';
-import { QueryAllCarsService } from './query-all-cars/query-all-cars.service';
 import { CreateCarComponent } from './submit-component/create-car/create-car.component';
 import { ChangeCarOwnerComponent } from './submit-component/change-car-owner/change-car-owner.component';
 import { CreateCarFormComponent } from './submit-component/create-car/create-car-form/create-car-form.component';
@@ -29,8 +28,7 @@ import { SubmitComponent } from './submit-component/submit.component';
     FormsModule
   ],
   providers: [
-    ApiService,
-    QueryAllCarsService
+    ApiService
   ],
   bootstrap: [AppComponent]
 })
